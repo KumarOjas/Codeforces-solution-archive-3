8348f023a3fde7521ab2151407d4f6f698a7e214
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2022-05-02 15:16:28 +0530

    Create TripForMeal.cpp

---
+++ TripForMeal.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+
+ int main ()
+ {
+    long n;
+  
+    scanf ("%lld",&n);
+  
+
+ 
+   long a,b,c;
+  
+  scanf("%ld %ld %ld", &a, &b, &c);
+
+    
+   
+   if(n == 1){puts("0");}
+    
+     else{
+   
+       
+       
+       int d = (a < b) ? a : b;
+       
+       
+       
+       long total = d + (n - 2) * ((d < c) ? d : c);
+       
+         printf("%ld\n", total);
+    }
+
+    return 0;
+}
